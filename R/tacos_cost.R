dab570bc348d425a35ec8ab9263523523d3b6714
h-hahn <hmhahn19@gmail.com>
2023-08-18 14:27:14 -0700

    Initial commit

---
+++ R/tacos_cost.R
@@ -0,0 +1,12 @@
+#' Calculate total taco cost
+#'
+#' @param price Price per taco in dollars
+#' @param count Number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tacos_cost <- function(price, count) {
+  price * count
+}
---
+++ R/tito_fur_conversion.R
@@ -0,0 +1,12 @@
+#' Tito fur conversion
+#'
+#' @param fur Amount of tito fur
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tito_fur_conversion <- function(fur) {
+  message("This is not real money!!!")
+  paste0("$", fur * 0)
+}
